1c3ee790faf57cf59d1302daf3b6693cbea95f8d
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-09-13 14:15:15 +0000

    a

--- PP1_1.py
+++ PP1_1.py
@@ -1,13 +1,13 @@
 def q1():
-  #Write Assignment code here
-
+  #write Assignment code here
+  
 def q2():
   #Write Assignment code here
-  
+
 def q3():
   #Write Assignment code here
   
-def q4:
+def q4():
   #Write Assignment code here
 
 q1()
